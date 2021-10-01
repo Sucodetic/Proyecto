fc695f2262772e8237ba0d770ec726e8c66d6f34
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2021-09-30 21:39:47 -0500

    Se añade nueva funcionalidad de validacion

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,27 +7,41 @@ function agregarVentas() {
   let precio = document.getElementById("valorUnitario").value;
   let estado = document.getElementById("estadoProducto").value;
 
-  venta = {
-    id,
-    descripcion,
-    precio,
-    estado,
-  };
+  console.log(validarCampos(id, descripcion, precio));
 
-  ventas.push(venta);
+  if (validarCampos(id, descripcion, precio)) {
+    venta = {
+      id,
+      descripcion,
+      precio,
+      estado,
+    };
 
-  ventas.forEach((el) => {
-    $tbody.innerHTML += `<tr>
-    <td>${el.id}</td>
-    <td>${el.descripcion}</td>
-    <td>${el.precio}</td>
-    <td>${el.estado}</td>
-    <td><button>Borrar</button></td>
-    <td><button>Editar</button></td>
-</tr>`;
-  });
+    ventas.push(venta);
 
-  document.getElementById("idProducto").value = "";
-  document.getElementById("descripcionProducto").value = "";
-  document.getElementById("valorUnitario").value = "";
+    ventas.forEach((el) => {
+      $tbody.innerHTML += `<tr>
+      <td>${el.id}</td>
+      <td>${el.descripcion}</td>
+      <td>${el.precio}</td>
+      <td>${el.estado}</td>
+      <td><button>Borrar</button></td>
+      <td><button>Editar</button></td>
+  </tr>`;
+    });
+
+    document.getElementById("idProducto").value = "";
+    document.getElementById("descripcionProducto").value = "";
+    document.getElementById("valorUnitario").value = "";
+  } else {
+    alert("Todos los campos deben ser diligenciados");
+  }
+}
+
+function validarCampos(id, descripcion, precio) {
+  if (id !== "" && descripcion !== "" && precio !== "") {
+    return true;
+  } else {
+    return false;
+  }
 }
